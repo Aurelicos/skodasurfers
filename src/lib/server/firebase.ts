a5af866296f1919026909b709f0770309ce1bab0
Aurelius Theodor Klíma <aureliust@seznam.cz>
2023-09-28 16:42:23 +0200

    functions

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
 import type { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+import { cars } from '$lib/constants';
 import key from "./key.json";
 
 export const initializeFirebase = () => {
@@ -52,15 +53,7 @@ export async function addDataToDB(token: string, name: string) {
     return { success: true, message: null };
 }
 
-export async function uploadScoreAndMoney(token: string) {
-    const decodedToken = await decodeToken(token);
-    if (!decodedToken) {
-        return { success: false, message: "Invalid token" }
-    };
-    const uid = decodedToken.uid;
-}
-
-export async function getData(token: string) {
+export async function uploadData(token: string, score: number | 0, money: number | 0, cars: string[] | []) {
     const decodedToken = await decodeToken(token);
     if (!decodedToken) {
         return { success: false, message: "Invalid token" }
@@ -68,11 +61,26 @@ export async function getData(token: string) {
     const uid = decodedToken.uid;
     const db = admin.firestore();
     const userRef = db.collection('users').doc(uid);
+    await userRef.update({
+        score: score,
+        money: money,
+        cars: cars,
+    });
+}
+
+export async function getData(token: string): Promise<{ success: boolean, message: string | null, data: any }> {
+    const decodedToken = await decodeToken(token);
+    if (!decodedToken) {
+        return { success: false, message: "Invalid token", data: null }
+    };
+    const uid = decodedToken.uid;
+    const db = admin.firestore();
+    const userRef = db.collection('users').doc(uid);
     const data = (await userRef.get()).data() ?? {};
     if (!data.score || !data.money || !data.cars) {
-        return { success: false, message: "Missing values!"}
+        return { success: false, message: "Missing values!", data: null }
     } else {
-        return {success: true, message: null, data}
+        return { success: true, message: null, data }
     }
 }
 
@@ -81,4 +89,23 @@ export async function getName(uid: string) {
     const userRef = db.collection('users').doc(uid);
     const data = (await userRef.get()).data() ?? {};
     return data.name;
+}
+
+export async function buyCar(uid: string, car: string) {
+    const db = admin.firestore();
+    const userRef = db.collection('users').doc(uid);
+    const userData = (await userRef.get()).data() ?? {};
+    // @ts-ignore
+    const carPrice = cars[car];
+    if (!carPrice) throw new Error('Car does not exist');
+
+    const userCurrency = userData.currency ?? 0;
+    if (userCurrency < carPrice) throw new Error('User cannot afford this car');
+
+    const userCars = userData.cars ?? [];
+    userCars.push(car);
+    await userRef.update({
+        cars: userCars,
+        currency: userCurrency - carPrice,
+    });
 }
